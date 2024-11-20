628616c74e5365bc6b858adbc3e4e78cdc50e4c0
Jem <0x0xjem@gmail.com>
2024-11-20 18:44:45 +0500

    Shutdown tests

---
+++ test/policies/Banker/shutdown.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {Banker} from "src/policies/Banker.sol";
+import {ROLESv1} from "src/modules/ROLES/ROLES.v1.sol";
+import {BankerTest} from "./BankerTest.sol";
+
+contract BankerShutdownTest is BankerTest {
+    // ======= Tests ======= //
+
+    // when the caller is not permissioned
+    // [X] it reverts
+    // when the policy is not active
+    // [X] the policy is still inactive
+    // when the policy is active
+    // [X] it sets the policy to inactive
+
+    function test_callerNotPermissioned_reverts() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(ROLESv1.ROLES_RequireRole.selector, bytes("admin"))
+        );
+        banker.shutdown();
+    }
+
+    function test_policyNotActive() public {
+        vm.prank(admin);
+        banker.shutdown();
+
+        // Assert
+        assertEq(banker.active(), false, "policy should be inactive");
+    }
+
+    function test_policyActive() public givenPolicyIsActive {
+        vm.prank(admin);
+        banker.shutdown();
+
+        // Assert
+        assertEq(banker.active(), false, "policy should be inactive");
+    }
+}
