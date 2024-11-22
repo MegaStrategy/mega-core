5402a09977853e45cc7b75db9be522892d80d242
Oighty <oighty6@gmail.com>
2024-11-22 11:11:08 -0600

    feat: port issuer to use audited BP oToken contracts

--- src/policies/Issuer.sol
+++ src/policies/Issuer.sol
@@ -8,25 +8,26 @@ import {TRSRYv1} from "src/modules/TRSRY/TRSRY.v1.sol";
 import {TOKENv1} from "src/modules/TOKEN/TOKEN.v1.sol";
 import {ROLESv1, RolesConsumer} from "src/modules/ROLES/OlympusRoles.sol";
 
-// Other Local
-import {oToken} from "src/misc/oToken.sol";
-
 import {ERC20} from "solmate-6.8.0/tokens/ERC20.sol";
 import {Timestamp} from "axis-core-1.0.1/lib/Timestamp.sol";
+import {TransferHelper} from "src/lib/TransferHelper.sol";
+
+import {FixedStrikeOptionToken as oToken} from "src/lib/oTokens/FixedStrikeOptionToken.sol";
+import {IFixedStrikeOptionTeller as oTeller} from "src/lib/oTokens/IFixedStrikeOptionTeller.sol";
 
 /// @title  Issuer
 /// @notice Policy that manages issuance of MSTR and options tokens
 contract Issuer is Policy, RolesConsumer {
     using Timestamp for uint48;
+    using TransferHelper for ERC20;
 
     // ========== ERRORS ========== //
 
     error InvalidParam(string name);
-    error oTokenExpired();
 
     // ========== EVENTS ========== //
 
-    event oTokenCreated(address indexed oToken, uint48 expiry, uint256 convertiblePrice);
+    event oTokenCreated(address indexed oToken);
 
     // ========== STATE ========== //
 
@@ -37,15 +38,17 @@ contract Issuer is Policy, RolesConsumer {
 
     // Local state
     bool public active;
+    oTeller public teller;
 
-    /// @notice whether the option token was issued by this contract
+    /// @notice Whether an oToken was created by this contract
     mapping(address => bool) public createdBy;
 
     // ========= POLICY SETUP ========= //
 
-    constructor(
-        Kernel kernel_
-    ) Policy(kernel_) {}
+    constructor(Kernel kernel_, address teller_) Policy(kernel_) {
+        // Set the teller to create oTokens from
+        teller = oTeller(teller_);
+    }
 
     /// @inheritdoc Policy
     function configureDependencies() external override returns (Keycode[] memory dependencies) {
@@ -96,71 +99,50 @@ contract Issuer is Policy, RolesConsumer {
         uint48 expiry_,
         uint256 convertiblePrice_
     ) external onlyRole("admin") returns (address token) {
-        // Create oToken
-        // Expiry and convertible price are validated in the token constructor
-        (string memory name, string memory symbol) = _computeNameAndSymbol(address(TOKEN), expiry_);
-
+        // Create oToken on oTeller
+        // Teller validates the inputs
         token = address(
-            new oToken(name, symbol, address(TOKEN), quoteToken_, expiry_, convertiblePrice_)
+            teller.deploy(
+                address(TOKEN), // payoutToken_ = MSTR
+                quoteToken_, // quoteToken_ = quoteToken
+                uint48(0), // eligible_ = immediately: TODO should we allow setting this?
+                expiry_, // expiry_ = expiry
+                address(TRSRY), // receiver_ = treasury (where funds go when options are exercised)
+                true, // call_ = true
+                convertiblePrice_ // strikePrice_ = convertiblePrice
+            )
         );
 
         // Mark the oToken as created by this contract
         createdBy[token] = true;
 
         // Emit event
-        emit oTokenCreated(token, expiry_, convertiblePrice_);
+        emit oTokenCreated(token);
     }
 
     function issueO(address token_, address to_, uint256 amount_) external onlyRole("admin") {
         // Validate that the oToken was created by this contract
         if (!createdBy[token_]) revert InvalidParam("token");
 
-        // Mint the oToken
-        oToken(token_).mint(to_, amount_);
+        // Amount must be greater than zero
+        if (amount_ == 0) revert InvalidParam("amount");
+
+        // Mint TOKENs to fund the oToken with
+        TOKEN.increaseMintApproval(address(this), amount_);
+        TOKEN.mint(address(this), amount_);
+
+        // Mint the oToken from the teller
+        // Approve the teller to pull the newly minted TOKENs
+        ERC20(address(TOKEN)).safeApprove(address(teller), amount_);
+        teller.create(oToken(token_), amount_);
+
+        // Send the oTokens to the recipient
+        ERC20(token_).safeTransfer(to_, amount_);
     }
 
-    function convertO(address token_, uint256 amount_) external {
-        // Validate that the oToken was created by this contract
-        if (!createdBy[token_]) revert InvalidParam("token");
-
-        // Get token data
-        (, ERC20 quoteToken, uint48 expiry, uint256 convertiblePrice) =
-            oToken(token_).getTokenData();
-
-        // Validate that the oToken has not expired
-        if (expiry <= block.timestamp) revert oTokenExpired();
-
-        // Calculate the amount of quote token required
-        uint256 quoteAmount = amount_ * convertiblePrice / 10 ** _tokenDecimals;
-
-        // Transfer the quote token from the caller to the treasury
-        // Requires approval to transfer
-        quoteToken.transferFrom(msg.sender, address(TRSRY), quoteAmount);
-
-        // Burn the oToken from the caller
-        // Requires approval to burn
-        oToken(token_).burnFrom(msg.sender, amount_);
-
-        // Mint TOKEN to the caller
-        TOKEN.mint(msg.sender, amount_);
-    }
-
-    /// @notice     Computes the name and symbol of an oToken
-    ///
-    /// @param      asset_      The address of the underlying token
-    /// @param      expiry_     The timestamp at which the option expires
-    /// @return     string      The name of the oToken
-    /// @return     string      The symbol of the oToken
-    function _computeNameAndSymbol(
-        address asset_,
-        uint48 expiry_
-    ) internal view returns (string memory, string memory) {
-        // Get the date components
-        (string memory year, string memory month, string memory day) = expiry_.toPaddedString();
-
-        return (
-            string(abi.encodePacked(ERC20(asset_).name(), " C ", year, "-", month, "-", day)),
-            string(abi.encodePacked("o", ERC20(asset_).symbol()))
-        );
+    function setTeller(
+        address teller_
+    ) external onlyRole("admin") {
+        teller = oTeller(teller_);
     }
 }
