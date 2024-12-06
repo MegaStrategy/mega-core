cd228eada2f28d0335aab339c17a8695c238e9ac
Jem <0x0xjem@gmail.com>
2024-12-06 18:04:11 +0400

    Remove minting functions from test script

--- script/Testing.s.sol
+++ script/Testing.s.sol
@@ -13,66 +13,6 @@ import {Banker} from "../src/policies/Banker.sol";
 import {Issuer} from "../src/policies/Issuer.sol";
 
 contract Testing is Script, WithEnvironment {
-    address constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
-    address constant USDC_MINTER = 0x2230393EDAD0299b7E7B59F20AA856cD1bEd52e1;
-    address payable constant WETH = payable(0x4200000000000000000000000000000000000006);
-
-    function setEthBalance(address to_, uint256 amount_) public {
-        string[] memory setBalanceInputs = new string[](5);
-        setBalanceInputs[0] = "cast";
-        setBalanceInputs[1] = "rpc";
-        setBalanceInputs[2] = "anvil_setBalance";
-        setBalanceInputs[3] = vm.toString(to_);
-        setBalanceInputs[4] = vm.toString(amount_);
-        vm.ffi(setBalanceInputs);
-
-        console2.log("ETH balance", address(to_).balance);
-    }
-
-    function depositEth(
-        uint256 amount_
-    ) public {
-        console2.log("Depositing ETH", amount_);
-
-        vm.startBroadcast();
-        WETHToken(WETH).deposit{value: amount_}();
-        vm.stopBroadcast();
-
-        console2.log("WETH balance", WETHToken(WETH).balanceOf(address(msg.sender)));
-    }
-
-    function mintToken(address token_, address tokenOwner_, address to_, uint256 amount_) public {
-        // TODO this isn't working, due to the "caller is not a minter" error
-        // Impersonate the token owner
-        string[] memory impersonateInputs = new string[](4);
-        impersonateInputs[0] = "cast";
-        impersonateInputs[1] = "rpc";
-        impersonateInputs[2] = "anvil_impersonateAccount";
-        impersonateInputs[3] = vm.toString(tokenOwner_);
-        vm.ffi(impersonateInputs);
-
-        // Mint the token
-        string[] memory mintInputs = new string[](9);
-        mintInputs[0] = "cast";
-        mintInputs[1] = "send";
-        mintInputs[2] = vm.toString(token_);
-        mintInputs[3] = "--from";
-        mintInputs[4] = vm.toString(tokenOwner_);
-        mintInputs[5] = "mint(address,uint256)(bool)";
-        mintInputs[6] = vm.toString(to_);
-        mintInputs[7] = vm.toString(amount_);
-        mintInputs[8] = "--unlocked";
-        vm.ffi(mintInputs);
-
-        // End impersonation
-        string[] memory endImpersonateInputs = new string[](4);
-        endImpersonateInputs[0] = "cast";
-        endImpersonateInputs[1] = "rpc";
-        endImpersonateInputs[2] = "anvil_stopImpersonatingAccount";
-        endImpersonateInputs[3] = vm.toString(tokenOwner_);
-        vm.ffi(endImpersonateInputs);
-    }
-
     function addAdmin(string calldata chain_, address admin_) external {
         _loadEnv(chain_);
 
@@ -114,8 +54,9 @@ contract Testing is Script, WithEnvironment {
         // Create the debt token
         uint48 maturity = uint48(block.timestamp + 1 days);
         vm.startBroadcast();
-        address debtToken =
-            Banker(_envAddressNotZero("mega.policies.Banker")).createDebtToken(WETH, maturity, 1e18);
+        address debtToken = Banker(_envAddressNotZero("mega.policies.Banker")).createDebtToken(
+            address(_envAddressNotZero("external.tokens.WETH")), maturity, 1e18
+        );
         vm.stopBroadcast();
         console2.log("debtToken", debtToken);
     }
@@ -127,15 +68,6 @@ contract Testing is Script, WithEnvironment {
         uint256 amount_
     ) external {
         _loadEnv(chain_);
-
-        // Only used on a fork
-        // // Deal the sender some ETH
-        // vm.startBroadcast();
-        // setEthBalance(msg.sender, 10 ether);
-        // vm.stopBroadcast();
-
-        // // Deposit ETH to obtain WETH
-        // depositEth(amount_);
 
         // Transfer the WETH amount to the Treasury
         vm.startBroadcast();
