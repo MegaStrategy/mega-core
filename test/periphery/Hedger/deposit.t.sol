58edcb4ca380ce7f57d5574bdc12dff710dfe3ae
Jem <0x0xjem@gmail.com>
2025-01-07 22:39:10 +0400

    deposit tests

--- test/periphery/Hedger/deposit.t.sol
+++ test/periphery/Hedger/deposit.t.sol
@@ -5,11 +5,57 @@ import {HedgerTest} from "./HedgerTest.sol";
 
 contract HedgerDepositTest is HedgerTest {
     // given the cvToken is not whitelisted
-    //  [ ] it reverts
+    //  [X] it reverts
     // given the caller has not approved this contract to spend the cvToken
-    //  [ ] it reverts
+    //  [X] it reverts
     // given the caller does not have sufficient balance of the cvToken
-    //  [ ] it reverts
-    // [ ] it transfers the cvToken to the Hedger from the caller
-    // [ ] it deposits the cvToken into the Morpho market on behalf of the caller
+    //  [X] it reverts
+    // [X] it transfers the cvToken to the Hedger from the caller
+    // [X] it deposits the cvToken into the Morpho market on behalf of the caller
+
+    function test_debtTokenNotWhitelisted_reverts() public {
+        // Expect revert
+        _expectInvalidDebtToken();
+
+        // Call
+        vm.prank(USER);
+        hedger.deposit(debtToken, DEBT_TOKEN_AMOUNT);
+    }
+
+    function test_debtTokenSpendingNotApproved_reverts() public givenDebtTokenIsWhitelisted {
+        // Expect revert
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+
+        // Call
+        vm.prank(USER);
+        hedger.deposit(debtToken, DEBT_TOKEN_AMOUNT);
+    }
+
+    function test_debtTokenInsufficientBalance_reverts()
+        public
+        givenDebtTokenIsWhitelisted
+        givenDebtTokenSpendingIsApproved(DEBT_TOKEN_AMOUNT)
+    {
+        // Expect revert
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+
+        // Call
+        vm.prank(USER);
+        hedger.deposit(debtToken, DEBT_TOKEN_AMOUNT);
+    }
+
+    function test_deposit()
+        public
+        givenDebtTokenIsWhitelisted
+        givenDebtTokenSpendingIsApproved(DEBT_TOKEN_AMOUNT)
+        givenDebtTokenIsIssued(DEBT_TOKEN_AMOUNT)
+    {
+        // Call
+        vm.prank(USER);
+        hedger.deposit(debtToken, DEBT_TOKEN_AMOUNT);
+
+        // Assertions
+        _assertUserBalances(0, 0);
+        _assertMorphoCollateral(DEBT_TOKEN_AMOUNT);
+    }
 }
