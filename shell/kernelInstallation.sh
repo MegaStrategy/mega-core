91dbc8c59a6cc1076977bd0d33a1c5608f54e27e
Jem <0x0xjem@gmail.com>
2025-02-20 22:25:25 +0400

    Upgrade kernel installation script

--- shell/kernelInstallation.sh
+++ shell/kernelInstallation.sh
@@ -1,66 +1,59 @@
 #!/bin/bash
 
-# Usage: ./kernelInstallation.sh --chain <chain> --env <env> --account <account> --broadcast <true|false>
+# Installs the modules and policies into the Kernel
+# Usage: ./kernelInstallation.sh
+#   --env <.env>
+#   --account <account>
+#   --broadcast <false>
 #
 # Environment variables:
-# - RPC_URL
-# - CHAIN
+# CHAIN:              Chain name to deploy to. Corresponds to names in "./script/env.json".
+# RPC_URL:            URL for the RPC node.
 
-# Iterate through named arguments
-# Source: https://unix.stackexchange.com/a/388038
-while [ $# -gt 0 ]; do
-    if [[ $1 == *"--"* ]]; then
-        v="${1/--/}"
-        declare $v="$2"
-    fi
+# Exit if there is an error
+set -e
 
-    shift
-done
+# Load named arguments
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+source $SCRIPT_DIR/lib/arguments.sh
+load_named_args "$@"
 
-# Get the name of the .env file or use the default
-ENV_FILE=${env:-".env"}
-echo "Sourcing environment variables from $ENV_FILE"
-
-# Load environment file
-set -a # Automatically export all variables
-source $ENV_FILE
-set +a # Disable automatic export
+# Load environment variables
+load_env
 
 # Apply defaults to command-line arguments
 BROADCAST=${broadcast:-false}
 
-# Check that the CHAIN environment variable is set
-if [ -z "$CHAIN" ]; then
-    echo "CHAIN environment variable is not set. Please set it in the .env file or provide it as an environment variable."
-    exit 1
-fi
+# Validate named arguments
+echo ""
+echo "Validating arguments"
+validate_text "$account" "No account specified. Provide the cast wallet after the --account flag."
 
-# Check that the forge account is set
-if [ -z "$account" ]; then
-    echo "Error: account is not set"
-    exit 1
-fi
+# Validate environment variables
+echo ""
+echo "Validating environment variables"
+validate_text "$CHAIN" "No chain specified. Specify the CHAIN in the $ENV_FILE file."
+validate_text "$RPC_URL" "No RPC URL specified. Specify the RPC_URL in the $ENV_FILE file."
 
-# Check that the RPC URL is set
-if [ -z "$RPC_URL" ]; then
-    echo "Error: RPC_URL is not set"
-    exit 1
-fi
+# Get the address of the cast wallet
+echo ""
+echo "Getting address for cast account $account"
+CAST_ADDRESS=$(cast wallet address --account $account)
 
-echo "Chain: $CHAIN"
-echo "Using RPC at URL: $RPC_URL"
-echo "Using forge account: $account"
+echo ""
+echo "Summary:"
+echo "  Deploy from account: $account"
+echo "  Sender: $CAST_ADDRESS"
+echo "  Chain: $CHAIN"
+echo "  RPC URL: $RPC_URL"
 
-# Set BROADCAST_FLAG based on BROADCAST
-BROADCAST_FLAG=""
-if [ "$BROADCAST" = "true" ] || [ "$BROADCAST" = "TRUE" ]; then
-    BROADCAST_FLAG="--broadcast"
-    echo "Broadcast: enabled"
-else
-    echo "Broadcast: disabled"
-fi
+# Validate and set forge script flags
+source $SCRIPT_DIR/lib/forge.sh
+set_broadcast_flag $BROADCAST
 
 forge script ./script/deploy/Deploy.s.sol:Deploy \
     --sig "kernelInstallation(string)()" $CHAIN \
-    --rpc-url $RPC_URL --account $account --slow -vvv \
+    --rpc-url $RPC_URL --account $account \
+    --sender $CAST_ADDRESS \
+    --slow -vvv \
     $BROADCAST_FLAG
